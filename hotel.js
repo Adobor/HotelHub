96eeb51e99054da1b947ec971df14d1ec0b7c5dc
Adobor <solomon.adobor@gmail.com>
2022-07-30 03:32:40 +0100

    adding scrol to top

---
+++ hotel.js
@@ -0,0 +1,12 @@
+mybutton = document.getElementsByClassName('header')
+function mov() {
+
+    document.body.scrollTop = 0;
+    // document.documentElement.scrollTop = 0;
+
+} document.addEventListener(click, mov())
+
+
+function scrol() {
+    document.getElementsByClassName('carosoul')
+} document
